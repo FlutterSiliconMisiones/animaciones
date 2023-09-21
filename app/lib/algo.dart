fe744e47aa0e02e772e0f450deeabda524a11cfc
Martin Peruchena <tinoper@gmail.com>
2023-09-21 20:11:16 -0300

    ROTATION Algo

---
+++ app/lib/algo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Algo extends StatelessWidget {
+  const Algo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.green,
+      ),
+    );
+  }
+}
--- app/lib/mover_algo_page.dart
+++ app/lib/mover_algo_page.dart
@@ -1,4 +1,6 @@
+import 'package:animaciones/algo.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
 class MoverAlgoPage extends StatelessWidget {
   const MoverAlgoPage({super.key});
@@ -20,15 +22,62 @@ class MoverAlgo extends StatefulWidget {
   State<MoverAlgo> createState() => _MoverAlgoState();
 }
 
-class _MoverAlgoState extends State<MoverAlgo> {
+class _MoverAlgoState extends State<MoverAlgo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotation;
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 80,
-      height: 80,
-      decoration: BoxDecoration(
-        color: Colors.green,
+  void initState() {
+    controller = AnimationController(
+      // vsync que se ejecute a los FPS necesarios
+      vsync: this,
+      duration: Duration(
+        seconds: 4,
       ),
     );
+
+    rotation = Tween(
+      begin: 0.0,
+      end: 2 * Math.pi,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeOut,
+      ),
+    );
+
+    /// Si se ha terminado la app le damos reset al controller para dejarlo
+    controller.addListener(() {
+      // print('Status: ${ controller.status }');
+      if (controller.status == AnimationStatus.completed) {
+        // controller.repeat();
+        // controller.reverse();
+        controller.reset();
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    controller.forward();
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: rotation.value,
+          child: Algo(),
+        );
+      },
+      child: Algo(),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
