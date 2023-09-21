58661f490d5465aacf6268840e0384562e609ebd
Martin Peruchena <tinoper@gmail.com>
2023-09-21 19:00:32 -0300

    AnimatedOpacity Sample

---
+++ app/lib/fade_in_demo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const owlUrl =
+    'https://raw.githubusercontent.com/flutter/website/main/src/assets/images/docs/owl.jpg';
+
+class FadeInDemo extends StatefulWidget {
+  const FadeInDemo({super.key});
+
+  @override
+  State<FadeInDemo> createState() => _FadeInDemoState();
+}
+
+class _FadeInDemoState extends State<FadeInDemo> {
+  double opacity = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Column(children: <Widget>[
+      Image.network(owlUrl, height: height * 0.8),
+      TextButton(
+        child: const Text(
+          'Show Details',
+          style: TextStyle(color: Colors.blueAccent),
+        ),
+        onPressed: () => setState(() {
+          opacity = 1;
+        }),
+      ),
+      AnimatedOpacity(
+        duration: const Duration(seconds: 2),
+        opacity: opacity,
+        child: const Column(
+          children: [
+            Text('Type: Owl'),
+            Text('Age: 39'),
+            Text('Employment: None'),
+          ],
+        ),
+      )
+    ]);
+  }
+}
